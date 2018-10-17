ec4175f3eb219d64d948541d7e75ab59185080df
nahtechnology <antonio.alonso@nahtechnology.com>
2018-10-17 16:55:33 -0500

    se agrega la navegacion implicita

---
+++ src/main/java/ingles/com/navegacion/Indice.java
@@ -0,0 +1,13 @@
+package ingles.com.navegacion;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Model;
+
+@Model
+public class Indice {
+
+    public String ejecucionPrueba() {
+        //codigo java
+        return "pruebas";
+    }
+}
