1cc85d5942f15332dfeb0d49a3a90045592f1cf8
Maugg <maugg76@gmail.com>
2018-10-17 17:25:30 -0500

    Se agrego navegacion alumnos implicita

---
+++ src/main/java/ingles/com/navegacion/NaveImplici.java
@@ -0,0 +1,15 @@
+package ingles.com.navegacion;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Model;
+
+
+@Model
+public class NaveImplici {
+	public String navegacionAlumnos() {
+        //codigo java
+        return "impliciAlumnos";
+
+}
+	
+}
