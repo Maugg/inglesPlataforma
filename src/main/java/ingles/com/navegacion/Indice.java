7b2a399b23e67cc9ffb0650a2344ea6fcfffeb8e
nahtechnology <antonio.alonso@nahtechnology.com>
2018-10-19 13:21:36 -0500

    se agrego if de formulario

--- src/main/java/ingles/com/navegacion/Indice.java
+++ src/main/java/ingles/com/navegacion/Indice.java
@@ -1,14 +1,29 @@
 package ingles.com.navegacion;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Model;
 
 @Model
 public class Indice {
 
+    private int valor;
+
+    @PostConstruct
+    public void initi() {
+        valor = 2;
+    }
+
     public String ejecucionPrueba() {
         //codigo java
         return "pruebas";
     }
 
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
 }
